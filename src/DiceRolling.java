13ba1e8708b365085184bc13a4b67ead9adb7784
nicksoule <nicksoule@live.com>
2018-01-25 15:47:18 -0500

    second commit with corrected changes to roll methods random gen.

--- src/DiceRolling.java
+++ src/DiceRolling.java
@@ -8,11 +8,13 @@ public class DiceRolling {
 		int diceSides;
 		String cont;
 		int rollCount = 1;
-		
+
+		// Start program by obtaining user input for diceSides
 		System.out.println("Welcome to the Grand Circus Casion!\n");
 		System.out.println("How many sides should each die have? ");
 		diceSides = scan.nextInt();
-		
+
+		// do while loop "rolls" dice and prints the number of rolls and roll result
 		do {
 			System.out.println("Roll " + rollCount + ":");
 			System.out.println(diceRoll1(diceSides));
@@ -21,27 +23,24 @@ public class DiceRolling {
 			cont = scan.next();
 			++rollCount;
 		} while (cont.equalsIgnoreCase("y"));
-		
+
 		System.out.println("Thanks for playing!");
-		
-		
-		scan.close(); 
+		scan.close();
 	}
-	
+	// new method for roll 1
 	public static int diceRoll1(int diceSides) {
-		//Random rand = new Random();
-		//int roll1 = rand.nextInt(diceSides) + 1;
-		int roll1 = (int) Math.random() * diceSides + 1; 
-		return roll1;	
-		
+		Random rand = new Random();
+		int roll1 = rand.nextInt(diceSides) + 1;
+		// int roll1 = (int) (Math.random() * diceSides + 1);
+		return roll1;
+
 	}
-	
+	// new method for roll 2
 	public static int diceRoll2(int diceSides) {
-		//Random rand = new Random();
-		//int roll2 = rand.nextInt(diceSides) + 1;
-		int roll2 = (int) (Math.random() * diceSides + 1);
-		return roll2;	
+		Random rand = new Random();
+		int roll2 = rand.nextInt(diceSides) + 1;
+		// int roll2 = (int) (Math.random() * diceSides + 1);
+		return roll2;
 	}
-	
 
 }
